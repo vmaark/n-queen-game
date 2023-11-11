2cf35300381031dad2af42e36318f9e769160cdd
Mark Vujevits <vujevits.mark@gmail.com>
2023-11-11 19:11:25 +0300

    more tests and conditions

--- src/NQueenTournament.sol
+++ src/NQueenTournament.sol
@@ -13,11 +13,11 @@ contract NQueenTournament {
 
     struct UserState {
         bool success;
-        uint8 round;
+        uint8 queensInRound;
     }
 
     uint256 public tournamentCounter;
-    uint8 public round = 2;
+    uint8 public queensInRound = 2;
 
     uint256 public startTimestamp;
     uint8 public numberOfSolvers;
@@ -25,40 +25,42 @@ contract NQueenTournament {
 
     function submitSolution(bytes[] memory proof, uint8 _round) public {
         // if the previous game is over, start a new one
-        if (startTimestamp + (round - 1) * 10 minutes + 10 minutes < block.timestamp) {
+        if (startTimestamp + (queensInRound - 1) * 10 minutes + 10 minutes < block.timestamp) {
             startTimestamp = block.timestamp;
-            round = 2;
+            queensInRound = 2;
             tournamentCounter++;
         }
 
         // if the previous round is over, start a new one
-        if (startTimestamp + (round - 1) * 10 minutes < block.timestamp) {
-            round++;
+        if (startTimestamp + (queensInRound - 1) * 10 minutes < block.timestamp) {
+            queensInRound++;
         }
 
-        require(players[tournamentCounter][msg.sender].round != round, "Already guessed for this round");
+        require(queensInRound == _round, "Incorrect round");
+        require(players[tournamentCounter][msg.sender].queensInRound != queensInRound, "Already guessed for this round");
         require(
-            round == 2 || players[tournamentCounter][msg.sender].round == round - 1, "Haven't submitted previous round"
+            queensInRound == 2 || players[tournamentCounter][msg.sender].queensInRound == queensInRound - 1,
+            "Haven't submitted previous round"
         );
-        require(round == 2 || players[tournamentCounter][msg.sender].success, "Failed to solve previous round");
+        require(queensInRound == 2 || players[tournamentCounter][msg.sender].success, "Failed to solve previous round");
 
-        (bool success) = validateSolution(proof, round);
-        players[tournamentCounter][msg.sender] = UserState(success, round);
+        (bool success) = validateSolution(proof, queensInRound);
+        players[tournamentCounter][msg.sender] = UserState(success, queensInRound);
 
-        emit SolutionSubmitted(msg.sender, tournamentCounter, round, success);
+        emit SolutionSubmitted(msg.sender, tournamentCounter, queensInRound, success);
 
         if (success) {
             numberOfSolvers++;
         }
 
         if (numberOfSolvers == 10) {
-            round++;
+            queensInRound++;
             numberOfSolvers = 0;
         }
     }
 
     function hasFinished() public view returns (bool) {
-        return startTimestamp + (round - 1) * 10 minutes < block.timestamp;
+        return startTimestamp + (queensInRound - 1) * 10 minutes < block.timestamp;
     }
 
     function validateSolution(bytes[] memory proof, uint256 _round) private returns (bool) {
--- test/NQueenTournament.t.sol
+++ test/NQueenTournament.t.sol
@@ -12,61 +12,156 @@ contract TournamentTest is Test {
     }
 
     function test_canStartTournament() public {
-        tournament.submitSolution(new bytes[](0), 0);
-        assertEq(tournament.round(), 2);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        assertEq(tournament.queensInRound(), 2);
         assertEq(tournament.tournamentCounter(), 0);
+    }
+
+    function test_cannotSubmitSolutionTwice() public {
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        assertEq(tournament.queensInRound(), 2);
+        assertEq(tournament.tournamentCounter(), 0);
+
+        uint8 rounds = tournament.queensInRound();
+        vm.expectRevert("Already guessed for this round");
+        tournament.submitSolution(new bytes[](0), rounds);
     }
 
     function test_doesntStartNewRoundFor10Minutes() public {
         address player2 = vm.addr(2);
-        tournament.submitSolution(new bytes[](0), 0);
-        assertEq(tournament.round(), 2);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        assertEq(tournament.queensInRound(), 2);
         assertEq(tournament.tournamentCounter(), 0);
 
         vm.warp(10 minutes - 1 seconds);
 
         vm.startPrank(player2);
-        tournament.submitSolution(new bytes[](0), 0);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
         vm.stopPrank();
-        assertEq(tournament.round(), 2);
+        assertEq(tournament.queensInRound(), 2);
         assertEq(tournament.tournamentCounter(), 0);
     }
 
+    function test_cannotSubmitSolutionForIncorrectRound() public {
+        uint8 rounds = tournament.queensInRound() + 1;
+        vm.expectRevert("Incorrect round");
+        tournament.submitSolution(new bytes[](0), rounds);
+    }
+
     function test_startsNewRoundAfter10Minutes() public {
-        tournament.submitSolution(new bytes[](0), 0);
-        assertEq(tournament.round(), 2);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        assertEq(tournament.queensInRound(), 2);
         assertEq(tournament.tournamentCounter(), 0);
 
         vm.warp(10 minutes + 1 seconds);
 
-        tournament.submitSolution(new bytes[](0), 0);
-        assertEq(tournament.round(), 3);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound() + 1);
+        assertEq(tournament.queensInRound(), 3);
         assertEq(tournament.tournamentCounter(), 0);
     }
 
     function test_cannotSubmitInNextRoundIfDidntSolveEarlier() public {
         address player2 = vm.addr(2);
-        tournament.submitSolution(new bytes[](0), 0);
-        assertEq(tournament.round(), 2);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        assertEq(tournament.queensInRound(), 2);
         assertEq(tournament.tournamentCounter(), 0);
 
         vm.warp(10 minutes + 1 seconds);
 
         vm.startPrank(player2);
+        uint8 rounds = tournament.queensInRound() + 1;
         vm.expectRevert("Haven't submitted previous round");
-        tournament.submitSolution(new bytes[](0), 0);
+        tournament.submitSolution(new bytes[](0), rounds);
         vm.stopPrank();
     }
 
     function testPreviousTournamentOverAfter20mins() public {
-        tournament.submitSolution(new bytes[](0), 0);
-        assertEq(tournament.round(), 2);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        assertEq(tournament.queensInRound(), 2);
         assertEq(tournament.tournamentCounter(), 0);
 
         vm.warp(20 minutes + 1 seconds);
 
-        tournament.submitSolution(new bytes[](0), 0);
-        assertEq(tournament.round(), 2);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        assertEq(tournament.queensInRound(), 2);
         assertEq(tournament.tournamentCounter(), 1);
+    }
+
+    function testNextRoundTriggeredAt10thSubmit() public {
+        address player2 = vm.addr(2);
+        address player3 = vm.addr(3);
+        address player4 = vm.addr(4);
+        address player5 = vm.addr(5);
+        address player6 = vm.addr(6);
+        address player7 = vm.addr(7);
+        address player8 = vm.addr(8);
+        address player9 = vm.addr(9);
+        address player10 = vm.addr(10);
+        address player11 = vm.addr(11);
+
+        vm.startPrank(player2);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        assertEq(tournament.queensInRound(), 2);
+        assertEq(tournament.tournamentCounter(), 0);
+
+        vm.startPrank(player3);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        vm.stopPrank();
+        assertEq(tournament.queensInRound(), 2);
+        assertEq(tournament.tournamentCounter(), 0);
+
+        vm.startPrank(player4);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        vm.stopPrank();
+        assertEq(tournament.queensInRound(), 2);
+        assertEq(tournament.tournamentCounter(), 0);
+
+        vm.startPrank(player5);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        vm.stopPrank();
+        assertEq(tournament.queensInRound(), 2);
+        assertEq(tournament.tournamentCounter(), 0);
+
+        vm.startPrank(player6);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        vm.stopPrank();
+        assertEq(tournament.queensInRound(), 2);
+        assertEq(tournament.tournamentCounter(), 0);
+
+        vm.startPrank(player7);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        vm.stopPrank();
+        assertEq(tournament.queensInRound(), 2);
+        assertEq(tournament.tournamentCounter(), 0);
+
+        vm.startPrank(player8);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        vm.stopPrank();
+        assertEq(tournament.queensInRound(), 2);
+        assertEq(tournament.tournamentCounter(), 0);
+
+        vm.startPrank(player9);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        vm.stopPrank();
+        assertEq(tournament.queensInRound(), 2);
+        assertEq(tournament.tournamentCounter(), 0);
+
+        vm.startPrank(player10);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        vm.stopPrank();
+        assertEq(tournament.queensInRound(), 2);
+        assertEq(tournament.tournamentCounter(), 0);
+
+        vm.startPrank(player11);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        vm.stopPrank();
+        assertEq(tournament.queensInRound(), 3);
+        assertEq(tournament.tournamentCounter(), 0);
+
+        vm.startPrank(player11);
+        tournament.submitSolution(new bytes[](0), tournament.queensInRound());
+        vm.stopPrank();
+        assertEq(tournament.queensInRound(), 3);
+        assertEq(tournament.tournamentCounter(), 0);
     }
 }
